92459d94079637f5084188664e51b249cf010fc5
luis_gonzalez <luis.gonzalez@expertla.com>
2018-07-21 21:23:07 -0500

    se agrega componente menu y se configura parte de las rutas

--- facturacion-web/src/app/app.module.ts
+++ facturacion-web/src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: MenuComponent, pathMatch:'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
